8a126478ec7e256c0275715189c5f8f88a12638d
Suning Yao <1651500561@qq.com>
2021-07-02 09:41:21 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://blog.suningyao.com/docs/Blockchain/erc"
             target="_blank"
             rel="noopener noreferrer"
           >
